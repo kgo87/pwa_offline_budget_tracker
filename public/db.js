333a7fe27d79599e4e425e99979884808ef65d3d
Kat Gomozova <kathy.gomozova@gmail.com>
2021-02-11 21:32:49 -0800

    adding reference to db.js to html

--- public/db.js
+++ public/db.js
@@ -1,10 +1,9 @@
 // const { response } = require("express");
 
 let db;
-// create a new db request for a "budget" database.
+// create a new indexedDB request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 request.onupgradeneeded = function (event) {
-  // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   const pendingStore = db.createObjectStore( "pendingList", {
     autoIncrement: true
@@ -22,22 +21,14 @@ request.onerror = function (event) {
   });
 };
 function saveRecord( record ) {
-  // create a transaction on the pending db with readwrite access
-  // access your pending object store
-  // add record to your store with add method.
   console.log( record );
   const transaction = db.transaction( ["pendingList"], "readwrite" );
   const pendingListStore = transaction.objectStore( "pendingList" );
-  // const pendingIndex = pendingListStore.index( "pendingIndex" );
   pendingListStore.add( record ); 
 }
 function checkDatabase() {
-  // open a transaction on your pending db
-  // access your pending object store
-  // get all records from store and set to a variable
   const transaction = db.transaction(["pendingList"], "readwrite");
   const pendingListStore = transaction.objectStore("pendingList");
-  // const pendingIndex = pendingListStore.index( "pendingIndex" );
   const getAll = pendingListStore.getAll();
   console.log(pendingListStore);
   getAll.onsuccess = function () {
@@ -52,9 +43,6 @@ function checkDatabase() {
       })
         .then((response) => response.json())
         .then(() => {
-          // if successful, open a transaction on your pending db
-          // access your pending object store
-          // clear all items in your store
           const transaction = db.transaction(["pendingList"], "readwrite");
           const pendingListStore = transaction.objectStore("pendingList");
           pendingListStore.clear();
